998bdd3adf311c6ea592f6f4d2679fe8f5a90e84
LinaArdila11 <112731928+LinaArdila11@users.noreply.github.com>
2022-11-02 17:44:51 -0500

    Add files via upload

---
+++ Persona.py
@@ -0,0 +1,38 @@
+class Persona:
+    
+    def __init__(self):
+        
+        self.nombre=" "
+        self.edad= int
+        self.genero=" "
+        self.volumen=" "
+        self.bigote=False
+        
+    def gritar (self):
+        pass
+        
+    
+    def bailar (self):
+        pass
+        
+    def gritar (self):
+        pass
+    
+sujetoA=Persona()
+sujetoA.nombre="Camilo"
+sujetoA.edad=20
+sujetoA.genero="Pansexual"
+sujetoA.bigote= False
+
+sujetoB=Persona()
+sujetoB.nombre="Cristian"
+sujetoB.edad=56
+sujetoB.genero="Maculino"
+sujetoB.bigote= False
+
+sujetoC=Persona()
+sujetoC.nombre="lina"
+sujetoC.edad=19
+sujetoC.genero="Femenino"
+sujetoC.bigote= False
+        
